1b39d14c982c083b40256506555f3f4b27cc0f34
zda9250 <zda9250@rit.edu>
2025-03-10 02:20:24 -0400

    Add in eleventy build yml file

--- .eleventy.js
+++ .eleventy.js
@@ -47,7 +47,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output: "public",
+            output: "docs",
         }
     };
 };
