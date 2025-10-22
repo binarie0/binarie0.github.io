721bd28c5adaca9fe5dd086b6d0e302cce6e1413
zda9250 <zda9250@rit.edu>
2025-10-21 20:42:10 -0400

    starting fresh

--- src/_data/links.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'LinkedIn': 'https://www.linkedin.com/in/zayers1/',
-    'BlueSky': 'https://bsky.app/profile/binarie0.bsky.social',
-    'osu': 'https://osu.ppy.sh/users/binarie'
-}
