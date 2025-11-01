eae83961a457002f33bad92d18c029c673164143
zda9250 <zda9250@rit.edu>
2025-10-31 23:20:04 -0400

    add the start of raycasting and add resizing

--- js/bg.js
+++ js/bg.js
@@ -16,10 +16,37 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 let node = document.body.appendChild( renderer.domElement );
 node.id = "c";
 
+let pointer = new THREE.Vector2();
 
-function animate() {
+function render()
+{
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
+
+//animation loop
+function animate() {
+    render();
+}
+
+//resizes the window when called
+function onWindowResize()
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", onWindowResize);
+
+//sets the raycast starting 2d pointer
+//(NOTE)::This is from one of the demos on the Threejs.org page
+function onMouseMove(event)
+{
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+document.addEventListener( 'pointermove', onMouseMove );
+
 renderer.setAnimationLoop( animate );
