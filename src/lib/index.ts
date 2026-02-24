0c34b06e256b5640acee74fcef3ecaf8303feb11
zda9250 <zda9250@rit.edu>
2026-02-24 17:41:05 -0500

    add tailwind and a whole bunch of config files

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+import ThreeBackground from "./components/ThreeBackground.svelte";
+
+export
+{
+    ThreeBackground
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+  import { ThreeBackground } from "$lib";
+</script>
+
+<ThreeBackground></ThreeBackground>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
