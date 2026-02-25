c49c55c63881844a8b2279f0cbc55c5422898a2b
zda9250 <zda9250@rit.edu>
2026-02-24 21:02:21 -0500

    add some styling

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -5,13 +5,13 @@
   export let text = "";
 </script>
 
-<div class="w-1/2 pb-4 px-5">
+<div class="w-1/2 pb-12 px-5">
   <div
     class="rounded-2xl w-full bg-clip-border border-2 border-white overflow-clip"
   >
     <img {src} alt={srcAlt} />
   </div>
   <p>
-    <a class="text-lg pl-2" target="_blank" {href}>{text}</a>
+    <a class="text-lg pl-2 pb-4" target="_blank" {href}>{text}</a>
   </p>
 </div>
