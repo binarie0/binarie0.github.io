3b3b59c232633a6f42c2168806052cf93c841b09
zda9250 <zda9250@rit.edu>
2026-04-30 23:12:54 -0400

    remove beatmap as i cant get it to work

--- osu-preprocessor/main.ts
+++
@@ -1,9 +0,0 @@
-import { StoryboardDecoder } from "osu-parsers";
-
-const decoder = new StoryboardDecoder();
-
-decoder.decodeFromPath('static/osumaps/2475368 BlackY - PANAGIA/BlackY - PANAGIA (yf_bmp) [Effulgence].osu', 'static/osumaps/2475368 BlackY - PANAGIA/BlackY - PANAGIA (yf_bmp).osb')
-.then((sb) =>
-{
-    console.log();
-})
